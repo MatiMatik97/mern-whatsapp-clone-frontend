657b6b9b178e0d334076538038058a257d00268b
Mateusz <matimatik97@gmail.com>
2020-09-10 12:18:01 +0200

    🗳 DEV-001 - Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
-import './App.scss';
+import React from "react";
+import "./App.scss";
+import Sidebar from "./layout/Sidebar/Sidebar";
+import Chat from "./layout/Chat/Chat";
 
 const App: React.FC = () => {
   return (
-    <div className='app'>
-      App
+    <div className="app">
+      <div className="app__body">
+        <Sidebar />
+        <Chat />
+      </div>
     </div>
   );
 };
