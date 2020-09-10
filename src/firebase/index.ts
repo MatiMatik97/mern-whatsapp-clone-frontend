294a2e199d551b3040d879ea793bd2e7653b4708
Mateusz <matimatik97@gmail.com>
2020-09-10 11:07:55 +0200

    setup

---
+++ src/firebase/index.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAmIUBscuwh-wr8v8KmaVM1AENehT5UM6k",
+  authDomain: "whatsapp-clone-mern-80df8.firebaseapp.com",
+  databaseURL: "https://whatsapp-clone-mern-80df8.firebaseio.com",
+  projectId: "whatsapp-clone-mern-80df8",
+  storageBucket: "whatsapp-clone-mern-80df8.appspot.com",
+  messagingSenderId: "44327547724",
+  appId: "1:44327547724:web:054a17ce1dfbcf705cd5e9",
+};
