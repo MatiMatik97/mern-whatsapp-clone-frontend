908bde74a93c7ba5253511f7cfda8f7da76be5a5
MatiMatik97 <matimatik97@gmail.com>
2023-02-06 22:39:43 +0100

    🗳 DEV-008 - Guest Login

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./LoginPage.scss";
 import { Button } from "@material-ui/core";
 import WhatsappLogo from "./whatsapp_logo.png";
@@ -7,6 +7,21 @@ import { useAppContext } from "../../contexts/AppContext";
 
 const LoginPage: React.FC = () => {
   const [, dispatch] = useAppContext();
+
+  const signInAsGuest = async () => {
+    const randomSeed = Math.floor(Math.random() * 10000) + 10000;
+
+    const user = {
+      uid: `${randomSeed}`,
+      displayName: `Guest #${randomSeed}`,
+      photoURL: `https://avatars.dicebear.com/api/human/${randomSeed}.svg`,
+      email: null,
+      phoneNumber: null,
+      providerId: "client",
+    };
+
+    dispatch({ type: "SET_USER", payload: user });
+  };
 
   const signInWithGoogle = async () => {
     try {
@@ -17,6 +32,12 @@ const LoginPage: React.FC = () => {
     }
   };
 
+  useEffect(() => {
+    if (window.location.pathname !== "/") {
+      window.location.pathname = "/";
+    }
+  }, []);
+
   return (
     <div className="loginPage">
       <img src={WhatsappLogo} alt="" />
@@ -25,9 +46,15 @@ const LoginPage: React.FC = () => {
         <h1>Whatsapp Clone</h1>
       </div>
 
-      <Button type="submit" onClick={signInWithGoogle}>
-        Sign In With Google
-      </Button>
+      <div className="loginPage__wrapper">
+        <Button type="submit" onClick={signInAsGuest}>
+          Sign In As Guest
+        </Button>
+
+        <Button type="submit" onClick={signInWithGoogle}>
+          Sign In With Google
+        </Button>
+      </div>
     </div>
   );
 };
